7cf3d5ee7caf0350a6958e40deee7dbb6549e6b9
Eliel P. Ferreira <eliel.dev.99@gmail.com>
2024-03-03 23:25:00 -0300

    Change resource.

--- src/main/java/api/piadas/piadas/data/Piadas.java
+++ src/main/java/api/piadas/piadas/data/Piadas.java
@@ -2,12 +2,14 @@ package api.piadas.piadas.data;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 @Component
@@ -17,7 +19,8 @@ public class Piadas {
     public static List<PiadaDAO> loadPiadas(){
         try{
             TypeReference<List<PiadaDAO>> typeReference = new TypeReference<List<PiadaDAO>>() {};
-            InputStream inputStream = new FileInputStream(new File("src/main/resources/jokes.json")){};
+            ClassPathResource resource = new ClassPathResource("jokes.json");
+            InputStream inputStream = resource.getInputStream();
             return mapper.readValue(inputStream, typeReference);
         } catch (IOException e){
             System.out.println("Error:: "+ e.getMessage());
